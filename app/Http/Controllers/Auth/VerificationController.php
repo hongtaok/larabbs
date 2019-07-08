0042f1704f58e382a0b2da5fdce4f5ecb7ab55bd
rhinooo <13653668463@163.com>
2019-07-08 14:42:06 +0800

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -34,8 +34,11 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+    	// 所有的控制器动作都需要登录有才能访问
         $this->middleware('auth');
+        // 只有 verify 动作使用 signed 中间件进行认证 （signed 中间件是一种由框架提供的很方便的 URL 签名认证方式）
         $this->middleware('signed')->only('verify');
+        // 对 verify 和 resend 动作做了频率限制， throttle 中间件是框架提供的访问频率限制功能
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
