3b9575dfa4be514fb10e36db28ac38ef46ee4a0d
rhinooo <13653668463@163.com>
2019-07-17 17:27:45 +0800

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,41 @@ class User extends Authenticatable implements MustVerifyEmailContract
     	return $this->id == $model->user_id;
 	}
 
+	public function markAsRead() {
+    	$this->notification_count = 0;
+    	$this->save();
+    	$this->unreadNotifications->markAsRead();
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
