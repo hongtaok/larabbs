2ffca3226708899b6fb9f86cdf6bc5ca8d49ec3e
rhinooooo <136536684463@163.com>
2019-07-17 23:05:19 +0800

    邮件通知home

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -22,7 +22,7 @@ class TopicsController extends Controller
 
 	public function index(Request $request, Topic $topic)
 	{
-		$topics = $topic->withOrder($request->order)->paginate(1);
+		$topics = $topic->withOrder($request->order)->paginate(20);
 
 		return view('topics.index', compact('topics'));
 	}
--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -35,7 +35,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
     	// 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable) {
@@ -54,4 +54,14 @@ class TopicReplied extends Notification
 			'topic_title' => $topic->title,
 		];
 	}
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+            ->line('你的话题有新回复！')
+            ->action('查看回复', $url);
+    }
+
 }
