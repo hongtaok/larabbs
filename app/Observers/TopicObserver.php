e86372bec947c51877d1f9d4cb00b53dcdd92f10
rhinooo <13653668463@163.com>
2019-07-18 15:55:11 +0800

    删除评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,6 +22,11 @@ class TopicObserver
 		dispatch(new TranslateSlug($topic));
 	}
 
+	public function deleted(Topic $topic) {
+    	// 在模型监听器中， 数据库操作需避免再次触发 Eloquent 事件， 以免造成联动逻辑冲突， 所以我们这里用了 DB 类进行操作
+    	\DB::table('replies')->where('topic_id', $topic->id)->delete();
+	}
+
 
 
 
